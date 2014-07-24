6dd611ac40bb6ea57c4bc5e88fa1af69f145d427
pratik1390 <pratikparekh13@gmail.com>
2014-07-24 15:31:03 -0500

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
 ## 
-## Write a short comment describing this function
+## 
 ## makeCacheMatrix is a function which returns a list containing four functions
 ## get() returns the matrix itself
+## set(y) assignt value y to the parent environment'x and inverse to NULL
+## setinverse assigns inv to parent environ's inverse
+## getinverse basically returns the inverse value
 makeCacheMatrix <- function(x = matrix()) 
 {
         inverse<-NULL
@@ -17,15 +20,17 @@ makeCacheMatrix <- function(x = matrix())
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## In the above function, <- is used to assign in the current enviroment while <<-assigns it to the parent environment.
+## cachesolve in a nutshell checks if there is a cache inverse stored beforehand. 
+## If there is one, it directs return that value. 
+## Otherwise it calculates the inverse, stores it and returns the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverse<-x$getinverse()
         if(!is.null(inverse))
         {
-                ##message("getting cached data")
+                message("getting cached data")
                 return(inverse)
         }
         data<-x$get()
